f284e16d91e2f6546f886c18114bb277699525bc
matatbread@mailed.me.uk <matatbread@mailed.me.uk>
2019-01-03 15:07:13 +0000

    Tidy up example

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http') ;
-const fs = require('fs');
-
-http.createServer((req,res) => {
-    res.write  
-}).listen(8000,err => {
-  if (err) console.warn(err) ;
-  else console.log("Open http://localhost:8000/") ;
-}) ;
